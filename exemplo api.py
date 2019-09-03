ee243f12d8efa7f06236a4f6f68fc09f22f426ad
Giovani Mesquita <giovani.mesquita@grupobs.com.br>
2019-09-03 09:26:14 -0300

    Giovani_Mesquita(RBST7497) 190903092614

---
+++ exemplo api.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from flask import Flask, jsonify, abort, request
+from flask_cors import CORS, cross_origin
+
+import logging
+import pymysql
+import os
+
+app = Flask(__name__)
+CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*","methods":"POST,DELETE,PUT,GET,OPTIONS"}})
+
+def creatConnection():
+    # Read MySQL Environment Parameters
+    connectString = os.environ.get('MYSQLCS_CONNECT_STRING', 'localhost:3306/myDB') 
+    hostname = connectString[:connectString.index(":")]
+    database = connectString[connectString.index("/")+1:]
+    conn = pymysql.connect(host=hostname, 
+	                       port=int(os.environ.get('MYSQLCS_MYSQL_PORT', '3306')), 
+						   user=os.environ.get('MYSQLCS_USER_NAME', 'root'), 
+						   passwd=os.environ.get('MYSQLCS_USER_PASSWORD', ''), 
+						   db=database,
+						   cursorclass=pymysql.cursors.DictCursor)
+    return conn;
+
+@app.route('/')
+def index():
+    return 'The application is running!'
+	
+@app.route('/employees/setupdb')
+def setupDB():
+    conn = creatConnection()
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE EMPLOYEE (
+				  ID INTEGER NOT NULL AUTO_INCREMENT,
+				  FIRSTNAME VARCHAR(255),
+				  LASTNAME VARCHAR(255),
+				  EMAIL VARCHAR(255),
+				  PHONE VARCHAR(255),
+				  BIRTHDATE VARCHAR(10),
+				  TITLE VARCHAR(255),
+				  DEPARTMENT VARCHAR(255),
+				  PRIMARY KEY (ID)
+				  ); ''') 
+    conn.commit()
+    cur.close()
+    conn.close()
+    return 'The EMPLOYEE tables was created succesfully'
+	
+@app.route('/employees')
+def employees():
+    conn = creatConnection()
+    cur = conn.cursor()
+    cur.execute('''SELECT ID, FIRSTNAME, LASTNAME, EMAIL, PHONE, BIRTHDATE, TITLE, DEPARTMENT FROM EMPLOYEE''')
+    results = cur.fetchall()	
+    cur.close()
+    conn.close()
+    return jsonify( results)	
+
+@app.route('/employees/<int:employee_id>', methods=['GET'])
+def get_employee(employee_id):
+    conn = creatConnection()
+    cur = conn.cursor()
+    cur.execute('''SELECT ID, FIRSTNAME, LASTNAME, EMAIL, PHONE, BIRTHDATE, TITLE, DEPARTMENT FROM EMPLOYEE WHERE ID = %s'''%(employee_id))
+    rv = cur.fetchone()    
+    if rv is None:
+        abort(404)
+    cur.close()
+    conn.close()
+    return jsonify( rv)	
+
+@app.route('/employees', methods=['POST'])
+def create_employee():
+    conn = creatConnection()
+    cur = conn.cursor()
+    try:
+        cur.execute('''INSERT INTO EMPLOYEE (FIRSTNAME, LASTNAME, EMAIL, PHONE, BIRTHDATE, TITLE, DEPARTMENT) 
+	                VALUES('%s','%s','%s','%s','%s','%s','%s') '''%(request.json['firstName'],request.json['lastName'],
+				    request.json['email'],request.json['phone'],request.json['birthDate'],request.json['title'],request.json['dept']))    
+        conn.commit()
+        message = {'status': 'New employee record is created succesfully'}
+        cur.close()	 
+    except Exception as e:
+        logging.error('DB exception: %s' % e)
+        message = {'status': 'The creation of the new employee failed.'}
+    conn.close()
+    return jsonify(message)
+
+@app.route('/employees/<int:employee_id>', methods=['PUT'])
+def update_employee(employee_id):
+    conn = creatConnection()
+    cur = conn.cursor()
+    try:
+        cur.execute('''UPDATE EMPLOYEE SET FIRSTNAME='%s', LASTNAME='%s', EMAIL='%s', PHONE='%s', BIRTHDATE='%s', TITLE='%s', DEPARTMENT='%s' 
+	               WHERE ID=%s '''%(request.json['firstName'],request.json['lastName'],
+				   request.json['email'],request.json['phone'],request.json['birthDate'],request.json['title'],request.json['dept'],employee_id))    
+        conn.commit()
+        message = {'status': 'The employee record is updated succesfully'}
+        cur.close()	 
+    except Exception as e:
+        logging.error('DB exception: %s' % e)	
+        message = {'status': 'Employee update failed.'}
+    conn.close()
+    return jsonify(message)
+
+@app.route('/employees/<int:employee_id>', methods=['DELETE'])
+def delete_employee(employee_id):
+    conn = creatConnection()
+    cur = conn.cursor()
+    try:
+        cur.execute('''DELETE FROM EMPLOYEE WHERE ID=%s '''%(employee_id))    
+        message = {'status': 'The employee record is deleted succesfully'}
+        conn.commit()
+        cur.close()	 
+    except Exception as e:
+        logging.error('DB exception: %s' % e)	
+        message = {'status': 'Employee delete failed.'}
+    conn.close()
+    return jsonify(message)
+
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=os.environ.get('PORT', '8080'))
