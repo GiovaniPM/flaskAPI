f334a2e0451da155e164bf6d671319c666548cdf
Giovani Perotto Mesquita <giovanipm@gmail.com>
2020-02-05 15:25:37 -0300

    Giovani_Mesquita(RBST10164) 200205152537

--- exemploapi.py
+++ exemploapi.py
@@ -16,80 +16,30 @@ auth = HTTPBasicAuth()
 CORS(app)
 cors = CORS(app, resources={r"/api/*": {"origins": "*","methods":"POST,DELETE,PUT,GET,OPTIONS"}})
 
-# @auth.get_password
-# def get_password(username):
-#     if username == 'miguel':
-#         return 'python'
-#     if username == 'giovanipm':
-#         return 'loco'
-#     return None
-
-@auth.verify_password
-def verify_password(username, password):
-    api_url = 'http://api.corp-app-hlg.rbs.com.br/ad/user'
-    group_list = ["G-INT-Colaboradores"]
-    headers = {'Content-type': 'application/json'}
-    data_dict_to_be_send = {
-                "login": username,
-                "password": password,
-                "adgroup": group_list,
-                "application": "PortalApis"}
-    data_json_format = dumps(data_dict_to_be_send, ensure_ascii=False)
-    r = post(api_url, data=data_json_format, headers=headers)
-    output_req = "%s" % r
-    if output_req != '<Response [200]>':
-        return False
-    else:
-        return True
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    if username == 'giovanipm':
+        return 'loco'
+    return None
 
 @auth.error_handler
 def unauthorized():
     return jsonify( { 'error': 'Unauthorized access' } )
 
 def creatConnection():
-    try:
-        db_host = os.environ['DB_HOST']
-    except Exception:
-        db_host = 'ora12cdev-scan.rbs.com.br'
-    try:
-        db_port = int(os.environ['DB_PORT'])
-    except Exception:
-        db_port = 1544
-    try:
-        db_servicename = os.environ['DB_SERVICENAME']
-    except Exception:
-        db_servicename = 'jdehlg.rbs.com.br'
-    try:
-        db_user = os.environ['DB_USER']
-        db_pass = os.environ['DB_PASS']
-    except Exception:
-        db_user = 'USER_JDE_QUERY'
-        db_pass = 'djangov1v3'
-    conn_string = "\
-                   (DESCRIPTION =\
-                        (ADDRESS_LIST =\
-                            (ADDRESS = (PROTOCOL = TCP)\
-                                (HOST = %s)\
-                                (PORT = %s))\
-                            )\
-                        (CONNECT_DATA =\
-                            (SERVICE_NAME = %s)\
-                        )\
-                    )" % (db_host, str(db_port), db_servicename)
-    return cx_Oracle.connect(user=db_user, password=db_pass, dsn=conn_string, encoding='UTF-8')
-
-# def creatConnection():
-#     # Read MySQL Environment Parameters
-#     connectString = os.environ.get('MYSQLCS_CONNECT_STRING', 'localhost:3306/myDB') 
-#     hostname = connectString[:connectString.index(":")]
-#     database = connectString[connectString.index("/")+1:]
-#     conn = pymysql.connect(host=hostname, 
-# 	                       port=int(os.environ.get('MYSQLCS_MYSQL_PORT', '3306')), 
-# 						   user=os.environ.get('MYSQLCS_USER_NAME', 'root'), 
-# 						   passwd=os.environ.get('MYSQLCS_USER_PASSWORD', ''), 
-# 						   db=database,
-# 						   cursorclass=pymysql.cursors.DictCursor)
-#     return conn;
+    # Read MySQL Environment Parameters
+    connectString = os.environ.get('MYSQLCS_CONNECT_STRING', 'localhost:3306/myDB') 
+    hostname = connectString[:connectString.index(":")]
+    database = connectString[connectString.index("/")+1:]
+    conn = pymysql.connect(host=hostname, 
+ 	                       port=int(os.environ.get('MYSQLCS_MYSQL_PORT', '3306')), 
+ 						   user=os.environ.get('MYSQLCS_USER_NAME', 'root'), 
+ 						   passwd=os.environ.get('MYSQLCS_USER_PASSWORD', ''), 
+ 						   db=database,
+ 						   cursorclass=pymysql.cursors.DictCursor)
+    return conn;
 
 @app.route('/')
 def index():
@@ -131,36 +81,6 @@ def get_employee(employee_id):
     cur = conn.cursor()
     cur.execute('''SELECT ID, FIRSTNAME, LASTNAME, EMAIL, PHONE, BIRTHDATE, TITLE, DEPARTMENT FROM EMPLOYEE WHERE ID = %s'''%(employee_id))
     rv = cur.fetchone()    
-    if rv is None:
-        abort(404)
-    cur.close()
-    conn.close()
-    return jsonify(rv)	
-
-@app.route('/cic/<int:tax>', methods=['GET'])
-@auth.login_required
-def get_cic(tax):
-    conn = creatConnection()
-    cur = conn.cursor()
-    sql_string = '''SELECT\
-                        ABAN8,\
-                        ABAT1,\
-                        ABALKY,\
-                        ABALPH,\
-                        ALADD1,\
-                        ALADD2,\
-                        ALADDZ,\
-                        ALCTY1,\
-                        ALADDS,\
-                        ALCTR\
-                    FROM\
-                        PRODDTAXE.F0101\
-                    INNER JOIN PRODDTAXE.F0116\
-                        ON ALAN8 = ABAN8\
-                    WHERE\
-                        ABTAX = '%s' ''' % (tax)
-    cur.execute(sql_string)
-    rv = cur.fetchall()    
     if rv is None:
         abort(404)
     cur.close()
