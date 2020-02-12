7efc92b1c4f74f86e1aeb312366ab6f7c6cbac95
Giovani Perotto Mesquita <giovanipm@gmail.com>
2020-02-12 17:50:02 -0300

    Giovani_Mesquita(RBST10164) 200212175002

--- json_in_out.py
+++ json_in_out.py
@@ -9,10 +9,84 @@ import logging
 import os
 import datetime
 import json
+import re
 
 app = Flask(__name__)
 CORS(app)
 cors = CORS(app, resources={r"/api/*": {"origins": "*","methods":"POST,DELETE,PUT,GET,OPTIONS"}})
+
+def isCpfValid(cpf):
+    """ If cpf in the Brazilian format is valid, it returns True, otherwise, it returns False. """
+    # Check if type is str
+    if not isinstance(cpf,str):
+        return False
+    # Remove some unwanted characters
+    cpf = re.sub("[^0-9]",'',cpf)
+    # Checks if string has 11 characters
+    if len(cpf) != 11:
+        return False
+    sum = 0
+    weight = 10
+    """ Calculating the first cpf check digit. """
+    for n in range(9):
+        sum = sum + int(cpf[n]) * weight
+        # Decrement weight
+        weight = weight - 1
+    verifyingDigit = 11 -  sum % 11
+    if verifyingDigit > 9 :
+        firstVerifyingDigit = 0
+    else:
+        firstVerifyingDigit = verifyingDigit
+    """ Calculating the second check digit of cpf. """
+    sum = 0
+    weight = 11
+    for n in range(10):
+        sum = sum + int(cpf[n]) * weight
+        # Decrement weight
+        weight = weight - 1
+    verifyingDigit = 11 -  sum % 11
+    if verifyingDigit > 9 :
+        secondVerifyingDigit = 0
+    else:
+        secondVerifyingDigit = verifyingDigit
+    if cpf[-2:] == "%s%s" % (firstVerifyingDigit,secondVerifyingDigit):
+        return True
+    return False
+
+def isCnpjValid(cnpj):
+    """ If cnpf in the Brazilian format is valid, it returns True, otherwise, it returns False. """
+    # Check if type is str
+    if not isinstance(cnpj,str):
+        return False
+    # Remove some unwanted characters
+    cpf = re.sub("[^0-9]",'',cnpj)
+    # Checks if string has 11 characters
+    if len(cpf) != 14:
+        return False
+    sum = 0
+    weight = [5,4,3,2,9,8,7,6,5,4,3,2]
+    """ Calculating the first cpf check digit. """
+    for n in range(12):
+        value =  int(cpf[n]) * weight[n]
+        sum = sum + value
+    verifyingDigit = sum % 11
+    if verifyingDigit < 2 :
+        firstVerifyingDigit = 0
+    else:
+        firstVerifyingDigit = 11 - verifyingDigit
+    """ Calculating the second check digit of cpf. """
+    sum = 0
+    weight = [6,5,4,3,2,9,8,7,6,5,4,3,2]
+    for n in range(13):
+        sum = sum + int(cpf[n]) * weight[n]
+    verifyingDigit = sum % 11
+    if verifyingDigit < 2 :
+        secondVerifyingDigit = 0
+    else:
+        secondVerifyingDigit = 11 - verifyingDigit
+    if cpf[-2:] == "%s%s" % (firstVerifyingDigit,secondVerifyingDigit):
+        return True
+    return False
 
 #curl -X GET -i -H "Content-Type: application/json" -d "{\"id\":32,\"nome\":\"Lucas\",\"ano\":2020}" http://127.0.0.1:8080/employees
 @app.route('/employees', methods=['GET'])
@@ -32,6 +106,32 @@ def view_employee():
         objects_list.append(reg)
         json_result = json.dumps(objects_list)
         return jsonify(json_result)
+
+#curl -X GET -i -H "Content-Type: application/json" -d "{\"cic\":\"62256092020\"}" http://127.0.0.1:8080/cic
+#curl -X GET -i -H "Content-Type: application/json" -d "{\"cic\":\"30917504000131\"}" http://127.0.0.1:8080/cic
+@app.route('/cic', methods=['GET'])
+def view_cic():
+    objects_list = []
+    if request.json == None:
+        abort(404)
+    elif 'cic' not in request.json:
+        abort(404)
+    elif not isinstance(request.json['cic'],str):
+        abort(404)
+    elif len(request.json['cic']) == 11:
+        reg            = {}
+        reg['result' ] = isCpfValid(request.json['cic'])
+        objects_list.append(reg)
+        json_result = json.dumps(objects_list)
+        return jsonify(json_result)
+    elif len(request.json['cic']) == 14:
+        reg            = {}
+        reg['result' ] = isCnpjValid(request.json['cic'])
+        objects_list.append(reg)
+        json_result = json.dumps(objects_list)
+        return jsonify(json_result)
+    else:
+        abort(404)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.environ.get('PORT', '8080'))
