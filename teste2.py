009c68f53b53a1d82cc1344b29cbb4bd9b2711ff
Giovani Perotto Mesquita <giovanipm@gmail.com>
2020-02-12 15:03:21 -0300

    Giovani_Mesquita(RBST10164) 200212150321

--- teste2.py
+++ teste2.py
@@ -7,16 +7,25 @@ from requests import post
 
 import logging
 import os
+import json
 
 app = Flask(__name__)
 CORS(app)
 cors = CORS(app, resources={r"/api/*": {"origins": "*","methods":"POST,DELETE,PUT,GET,OPTIONS"}})
 
-#curl -X GET -i -H "Content-Type: application/json" -d "{\"id\":1}" http://127.0.0.1:8080/employees
+#curl -X GET -i -H "Content-Type: application/json" -d "{\"id\":1,\"nome\":\"Giovani\"}" http://127.0.0.1:8080/employees
 @app.route('/employees', methods=['GET'])
 def view_employee():
-    print('%s') % (str(request.json['id']))
-    return
+    objects_list = []
+    if request.json == None:
+        abort(404)
+    else:
+        reg        = {}
+        reg['01' ] = request.json['id']
+        reg['02' ] = request.json['nome']
+        objects_list.append(reg)
+        json_result = json.dumps(objects_list)
+        return jsonify(json_result)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.environ.get('PORT', '8080'))
